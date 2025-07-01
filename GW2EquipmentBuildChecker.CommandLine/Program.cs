08d9560621d6f0d72bbdf95a960f9c46757611d4
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-07-01 22:37:28 +0200

    Select a character

--- GW2EquipmentBuildChecker.CommandLine/Program.cs
+++ GW2EquipmentBuildChecker.CommandLine/Program.cs
@@ -15,10 +15,23 @@ namespace GW2EquipmentBuildChecker.CommandLine
             var characterNames = await api.GetCharactersNames();
 
             // 2. Pick 1 character
+            Console.WriteLine("Characters list:");
+            var characterIndex = 0;
             foreach (var characterName in characterNames)
             {
-                Console.WriteLine(characterName);
+                Console.WriteLine($"{characterIndex + 1}: {characterName}");
+                ++characterIndex;
             }
+
+            Console.WriteLine("\nPick a character by writing the number...");
+
+            if (!int.TryParse(Console.ReadLine(), out var characterChoice) && characterChoice > 0 && characterChoice <= characterNames.Length)
+            {
+                Console.WriteLine("Invalid character choice");
+                return;
+            }
+
+            var selectedCharacter = characterNames[characterChoice - 1];
 
             // 3. Get the list of builds
 
