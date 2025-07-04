20d67362ca9298ee337c8e1e348778433e61bcc4
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-07-04 22:33:38 +0200

    Compare specializations

--- GW2EquipmentBuildChecker.Core/BuildComparer.cs
+++ GW2EquipmentBuildChecker.Core/BuildComparer.cs
@@ -1,4 +1,5 @@
-﻿using GW2EquipmentBuildChecker.Core.GW2.Entities.Characters;
+﻿using GW2EquipmentBuildChecker.Core.GW2;
+using GW2EquipmentBuildChecker.Core.GW2.Entities.Characters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,16 +10,29 @@ namespace GW2EquipmentBuildChecker.Core
 {
     public static class BuildComparer
     {
-        public static List<string> CompareBuilds(Build sourceBuild, Build targetBuild)
+        public static async Task<List<string>> CompareBuilds(Build sourceBuild, Build targetBuild)
         {
             var differences = new List<string>();
             if (sourceBuild.Profession != targetBuild.Profession)
             {
-                differences.Add($"Profession mismatch: GW2 has '{sourceBuild.Profession}' , gw2skills has '{targetBuild.Profession}'");
+                differences.Add($"Profession mismatch: gw2skills has '{targetBuild.Profession}', GW2 has '{sourceBuild.Profession}'");
             }
             else
             {
+                var sourceSpecs = sourceBuild.Specializations.Select(s => s.Id);
+                var targetSpecs = targetBuild.Specializations.Select(s => s.Id);
+                var diffSourceSpecs = sourceSpecs.Except(targetSpecs);
+                var diffTargetSpecs = targetSpecs.Except(sourceSpecs);
 
+                var gw2Specializations = await GW2API.GetSpecializationsAsync();
+
+                if (diffSourceSpecs.Any() || diffTargetSpecs.Any())
+                {
+                    differences.Add($"Specialization mismatch: gw2skills has '{string.Join(", ", diffTargetSpecs.Select(s => gw2Specializations.First(ss => ss.Id == s).Name))}', GW2 has '{string.Join(", ", diffSourceSpecs.Select(s => gw2Specializations.First(ss => ss.Id == s).Name))}'");
+                }
+                else
+                {
+                }
             }
 
             return differences;
