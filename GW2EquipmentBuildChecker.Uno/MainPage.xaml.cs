b19d46ceae6911451f5424bc5ded968dd0030d33
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-07-07 15:20:53 +0200

    Show differences

--- GW2EquipmentBuildChecker.Uno/MainPage.xaml.cs
+++ GW2EquipmentBuildChecker.Uno/MainPage.xaml.cs
@@ -1,10 +1,15 @@
+using GW2EquipmentBuildChecker.Core;
 using GW2EquipmentBuildChecker.Core.GW2;
+using GW2EquipmentBuildChecker.Core.GW2.Entities.Characters;
+using GW2EquipmentBuildChecker.Core.GW2Skills;
+using System.Text;
 
 namespace GW2EquipmentBuildChecker.Uno;
 
 public sealed partial class MainPage : Page
 {
     GW2API GW2API;
+    GW2SkillsAPI GW2SkillsAPI = new GW2SkillsAPI();
 
     public MainPage()
     {
@@ -96,5 +101,45 @@ public sealed partial class MainPage : Page
         BuildUrlTextBox.Visibility = Visibility.Visible;
 
         CompareButton.Visibility = Visibility.Visible;
+    }
+
+    private async void CompareButton_Click(object sender, RoutedEventArgs e)
+    {
+        DifferencesTextBlock.Visibility = Visibility.Collapsed;
+
+        if (string.IsNullOrEmpty(BuildUrlTextBox.Text))
+        {
+            var dialog = new ContentDialog
+            {
+                XamlRoot = this.XamlRoot,
+                Title = "GW2Skills.net Build URL Required",
+                Content = "Please enter a GW2Skills.net build URL.",
+                CloseButtonText = "OK"
+            };
+            await dialog.ShowAsync();
+            return;
+        }
+
+        var gw2skillsBuild = await GW2SkillsAPI.GetBuildAsync(BuildUrlTextBox.Text);
+
+        // 6. Compare and find differences
+        var buildDifferences = BuildComparer.CompareBuilds(((BuildContainer)(BuildComboBox.SelectedItem)).Build, gw2skillsBuild);
+
+        var differencesText = new StringBuilder();
+        // 7. Tell what to change
+        if (buildDifferences.Count == 0)
+        {
+            differencesText.AppendLine("No differences found");
+        }
+        else
+        {
+            foreach (var difference in buildDifferences)
+            {
+                differencesText.AppendLine(difference);
+            }
+        }
+
+        DifferencesTextBlock.Text = differencesText.ToString();
+        DifferencesTextBlock.Visibility = Visibility.Visible;
     }
 }
