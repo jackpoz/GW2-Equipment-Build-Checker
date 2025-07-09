6dca6d8fe07d5a69f79609256d9aa3b69f388976
Giacomo Pozzoni <giacomopoz@gmail.com>
2025-07-09 13:32:45 +0200

    Compare heal, utility and elite skills

---
+++ GW2EquipmentBuildChecker.Core/GW2Skills/Entities/SkillsInfoContainer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GW2EquipmentBuildChecker.Core.GW2Skills.Entities
+{
+    internal class SkillsInfoContainer
+    {
+        public SkillContent[] Content { get; set; }
+    }
+
+    internal class SkillContent
+    {
+        public string[] Skill { get; set; }
+
+        public string GetSkillName(int skillId)
+        {
+            var row = Skill.First(s => s.StartsWith($"i={skillId}#"));
+            var startPrefix = "#n=";
+            var startIndex = row.IndexOf(startPrefix) + startPrefix.Length;
+            var endIndex = row.IndexOf("#d=", startIndex);
+            return row.Substring(startIndex, endIndex - startIndex);
+        }
+    }
+}
